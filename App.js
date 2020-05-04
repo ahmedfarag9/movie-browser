20c9e317a7f7fe20ce6c312faec41f2742647cd1
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-05-04 04:43:26 +0200

    Imported fetchMovie func from it's file

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import {fetchMovie} from './api'
 
 const styles = StyleSheet.create({
   container: {
@@ -9,6 +9,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+})
+
+
 export default class App extends React.Component {
 
 	state = {
