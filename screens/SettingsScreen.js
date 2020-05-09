b5c526072f6f8619747407130921e90d43bfd0b6
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-05-09 07:58:39 +0200

    Cleaned the code

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Button, View, StyleSheet, Text} from 'react-native'
 
-import Ionicons from 'react-native-vector-icons/Ionicons'
 
 
 const styles = StyleSheet.create({
