273f4a59af74729fbfcd0a781b8866b7eaf05537
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-05-09 08:01:56 +0200

    imported functions from other files

--- SearchForm.js
+++ SearchForm.js
@@ -1,2 +1,4 @@
 import React from 'react'
 import {Button, KeyboardAvoidingView, StyleSheet, TextInput, View, Text} from 'react-native'
+import {fetchMovie} from './api'
+//import {search} from './mockData'
