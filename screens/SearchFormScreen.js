ac5c2110a750161d2912ae4cdb97d4f1f66276a9
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-05-10 01:19:12 +0200

    Imported some libraries and local files

--- screens/SearchFormScreen.js
+++ screens/SearchFormScreen.js
@@ -1,3 +1,13 @@
+import React from 'react'
+import {Button, View, StyleSheet, Text} from 'react-native'
+import SearchForm from '../SearchForm'
+//import ScrollViewMovies from '../ScrollViewMovies'
+//import {fetchMovie} from '../api'
+import FlatListMovies from '../FlatListMovies'
+
+//const renderItem = movie => <Text style={styles.text}>{movie.name} {movie.year}</Text>
+
+
 const styles = StyleSheet.create({
 	container: {
 	  flex: 1,
