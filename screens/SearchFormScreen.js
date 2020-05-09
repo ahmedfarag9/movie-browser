a961341c5e0cba5682b7e05dd2fb205a4a485d7e
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-05-10 01:14:51 +0200

    Added some style

--- screens/SearchFormScreen.js
+++ screens/SearchFormScreen.js
@@ -1,3 +1,15 @@
-import React from 'react'
-import {Button, View, StyleSheet, Text} from 'react-native'
-import SearchForm from '../SearchForm'
+const styles = StyleSheet.create({
+	container: {
+	  flex: 1,
+	},
+	text: {
+		justifyContent: 'center',
+		//flex: 1,
+		marginTop: 50,
+		marginHorizontal: 50,
+		paddingHorizontal: 50,
+		//paddingVertical: 5,
+	  },
+	  
+  })
+  
