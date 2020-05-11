56c6e3d660e11dbb8318faaeffe9752ea0ab3511
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-05-11 04:35:02 +0200

    Added default class

--- screens/MovieDetailsScreen.js
+++ screens/MovieDetailsScreen.js
@@ -1,2 +1,8 @@
 import React from 'react'
 import {Button, View, StyleSheet, Text, Image, TouchableOpacity } from 'react-native'
+export default class MovieDetailsScreen extends React.Component {
+
+  state = {
+    movie: null,
+    showMovie: false,
+  }
