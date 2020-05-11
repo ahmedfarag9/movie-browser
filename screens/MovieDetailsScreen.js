9021e0c6444f1276f8f8ada6c5d53562349e8dd6
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-05-11 04:35:37 +0200

    Added some style

--- screens/MovieDetailsScreen.js
+++ screens/MovieDetailsScreen.js
@@ -1,5 +1,23 @@
 import React from 'react'
 import {Button, View, StyleSheet, Text, Image, TouchableOpacity } from 'react-native'
+const styles = StyleSheet.create({
+    container: {
+      //justifyContent: 'center',
+      flex: 1,
+      //marginTop: 20,
+      //marginHorizontal: 50,
+      paddingHorizontal: 10,
+      //paddingVertical: 5,
+    },
+    text: {
+      //textAlign: 'center',
+    },
+    image: {
+      
+    },    
+  })
+  
+
 export default class MovieDetailsScreen extends React.Component {
 
   state = {
