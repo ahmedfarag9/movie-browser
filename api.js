e83b89215a2c1221c5bdae6485e2455a958ba770
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-05-14 12:08:00 +0200

    handle recieved data

--- api.js
+++ api.js
@@ -12,6 +12,16 @@ export const fetchMovies = async (movieName) => {
     const movieNameTmp = "'" + movieName + "'"
 
 
+	const response = await fetch(url)
+    const results = await response.json()    
+	//console.log(results)
+
+	return results.Search.map(processMovies)
+	
+}
+
+
+
 export const fetchMovie = async () => {
     const response = await fetch('URL')
     const {results} = await response.json()
