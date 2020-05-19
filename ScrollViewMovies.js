caa5bdf777cb7adcbe104b42a552764d8bfa4622
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-05-19 11:47:40 +0200

    Added scroll view and mapping func

--- ScrollViewMovies.js
+++ ScrollViewMovies.js
@@ -3,3 +3,13 @@ import {ScrollView} from 'react-native'
 import PropTypes from 'prop-types'
 
 import Row from './Row'
+
+const ScrollViewMovies = props => (
+	<ScrollView key = {props.movies.imdbID} >{props.movies.map(movie => <Row {...movie} />)}</ScrollView>
+)
+
+ScrollViewMovies.PropTypes = {
+	movies: PropTypes.array
+}
+
+export default ScrollViewMovies
