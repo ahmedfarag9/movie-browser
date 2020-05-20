99f4866fc96c347deae300be48acdfd54733a3da
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-05-20 05:39:25 +0200

    Added a Flatlist component

--- FlatListMovies.js
+++ FlatListMovies.js
@@ -3,3 +3,15 @@ import {FlatList} from 'react-native'
 import propTypes from 'prop-types'
 
 import Row from './Row'
+
+const FlatListMovies = props => {
+
+  const tmpProp = props.onSelectMovie
+  //console.log(tmpProp)
+  const tmpMovies = props.movies
+
+
+  return (
+    <FlatList renderItem={({item}) => <Row {...item} onSelectMovie={tmpProp} />} data={tmpMovies}  keyExtractor={item => item.imdbID}/>
+  )
+}
