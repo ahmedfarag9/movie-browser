83d6405aace3864431123992413c4ad71c60a41a
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-05-21 03:50:54 +0200

    Added some style

--- Row.js
+++ Row.js
@@ -1,3 +1,17 @@
 import React from 'react'
 import{TouchableOpacity, StyleSheet, Text, View} from 'react-native'
 import propTypes from 'prop-types'
+
+const styles = StyleSheet.create({
+	row: {padding: 20},
+	//borderWidth: 1,
+	//borderColor: 'black',
+	//width: 10,
+	//hight: 10,
+	//minHight: 100,
+	//marginTop: 10,
+	marginHorizontal: 50,
+	paddingHorizontal: 50,
+	//paddingVertical: 5,
+	//borderRadius: 3,
+})
